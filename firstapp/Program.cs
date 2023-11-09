3e24a4b4bdd4d76b427f0f2f8915246bbe40804a
PragatiBangar <bangarpragati11@gmail.com>
2023-11-09 21:58:11 +0530

    fibonacci series

---
+++ firstapp/Program.cs
@@ -0,0 +1,26 @@
+﻿
+//fibonacci series
+static void ShowFibonacciSeriesNumber(int level)
+{
+    int firstNumber = 0;
+    int secondNumber = 1;
+    Console.Write(firstNumber + " , " + secondNumber + " , ");
+    int sum;
+    for (int i = 2; i < level; i++)
+    {
+        sum = firstNumber + secondNumber;
+        firstNumber = secondNumber;
+        secondNumber = sum;
+        Console.Write(secondNumber + ", ");
+    }
+}
+
+ShowFibonacciSeriesNumber(5);
+
+
+
+
+
+
+
+
---
+++ firstapp/number.cs
@@ -0,0 +1,21 @@
+// namespace FirstApp.num
+// {
+//     public class Number{
+
+//         int reverse=0;
+//         int rem;
+        
+//         public int GetSum(int num){
+
+//         while(num!=0){
+//         rem=num%10;
+//     // 8765%10           876%10          87%10              8%10
+//     // rem=5             rem=6           rem=7              rem=8
+//         reverse=reverse*10+rem;
+//        //0*10+5=5        5*10+6=56       56*10+7=560+7=567  567*10+8=5678
+//         num/=10; 
+//         return 0;
+//         }
+//     }
+//   }  
+// }
